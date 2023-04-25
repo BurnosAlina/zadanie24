e8cd00ca8b7cf9731ab08714db694232d7629baa
Alina Burnos <burnosalina@gmail.com>
2023-04-25 07:15:17 +0200

    zadanie24

---
+++ src/main/java/com/example/zadanie24/Zadanie24Application.java
@@ -0,0 +1,13 @@
+package com.example.zadanie24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zadanie24Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zadanie24Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zadanie24/Zadanie24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zadanie24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zadanie24ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
